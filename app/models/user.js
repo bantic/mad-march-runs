ef9965571ff263595c4fe68d06948b87647f504b
Cory Forsyth <cory.forsyth@gmail.com>
2015-03-15 20:12:34 -0400

    add isAdmin to user

--- app/models/user.js
+++ app/models/user.js
@@ -6,6 +6,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   email: DS.attr('string'),
   password: DS.attr('string'),
+  isAdmin: DS.attr('boolean'),
 
   canSelectTeams: DS.attr('boolean'),
   teams: DS.hasMany('team', {async:true})
