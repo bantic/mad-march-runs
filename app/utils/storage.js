ee53290248d6313d94994532ed6f97bd30c0d07b
Cory Forsyth <cory.forsyth@gmail.com>
2015-03-18 23:38:04 -0400

    localStorage shim

--- app/utils/storage.js
+++ app/utils/storage.js
@@ -1,5 +1,31 @@
 var serializedPrefix = '_object:';
 
+var setItem = (k,v) => window.localStorage.setItem(k,v);
+var getItem = (v) => window.localStorage.getItem(v);
+var removeItem = (v) => window.localStorage.removeItem(v);
+
+var storage = {};
+
+// memory-based local storage shim
+var defaultSetItem = (k,v) => storage[k] = v;
+var defaultGetItem = (v) => storage[v];
+var defaultRemoveItem = (v) => delete storage[v];
+
+function checkLocalStorage(){
+  try {
+    window.localStorage.setItem('_test',true);
+    return true;
+  } catch(e) {
+    return false;
+  }
+}
+
+if (!checkLocalStorage()) {
+  setItem    = defaultSetItem;
+  getItem    = defaultGetItem;
+  removeItem = defaultRemoveItem;
+}
+
 export function write(key, value) {
   var serialized;
   if (typeof value === 'object') {
@@ -7,11 +33,11 @@ export function write(key, value) {
   } else {
     serialized = value;
   }
-  return window.localStorage.setItem(key, serialized);
+  return setItem(key, serialized);
 }
 
 export function read(key) {
-  var serialized = window.localStorage.getItem(key);
+  var serialized = getItem(key);
   var value;
   if (typeof serialized === 'string' && serialized.indexOf(serializedPrefix) === 0){
     value = JSON.parse(serialized.slice(8));
@@ -22,7 +48,7 @@ export function read(key) {
 }
 
 export function remove(key) {
-  return window.localStorage.removeItem(key);
+  return removeItem(key);
 }
 
 export default {
